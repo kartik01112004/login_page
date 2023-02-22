3779a06b2f01c7e6c999442badac930edced9107
Kartik Goel <kartikgoel018@gmail.com>
2023-02-22 22:29:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:login_page/after%20login.dart';
+import 'login page.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: "AIzaSyAb7QlySSxbCVGYiCSYVLUP01ssl1rEbRY",
+        appId: "1:408956506917:web:a3cb13a98f8a20bb0580cc",
+        messagingSenderId: "408956506917",
+        projectId: "login-page-3f87a"));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: StreamBuilder(
+    stream: FirebaseAuth.instance.authStateChanges(),
+    builder: (context, snapshot) {
+    if (snapshot.hasData) {
+    return RESULT();
+    } else {
+    return LOGINPAGE();
+    }
+    },
+    ));
+  }
+}
+
